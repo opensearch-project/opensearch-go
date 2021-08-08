6d4438f96e759b50736258d76c6c9aa2a560c749
Vijayan Balasubramanian <balasvij@amazon.com>
2021-08-08 14:15:28 -0700

    Update license header
    Update license generator to only SPDX license.
    
    Signed-off-by: Vijayan Balasubramanian <balasvij@amazon.com>

--- internal/build/cmd/generate/commands/gensource/generator.go
+++ internal/build/cmd/generate/commands/gensource/generator.go
@@ -98,22 +98,14 @@ func (g *Generator) w(s string) {
 }
 
 func (g *Generator) genHeader() {
-	g.w(`// Licensed to Elasticsearch B.V. under one or more contributor
-// license agreements. See the NOTICE file distributed with
-// this work for additional information regarding copyright
-// ownership. Elasticsearch B.V. licenses this file to you under
-// the Apache License, Version 2.0 (the "License"); you may
-// not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+	g.w(`// SPDX-License-Identifier: Apache-2.0
 //
-//    http://www.apache.org/licenses/LICENSE-2.0
+// The OpenSearch Contributors require contributions made to
+// this file be licensed under the Apache-2.0 license or a
+// compatible open source license.
 //
-// Unless required by applicable law or agreed to in writing,
-// software distributed under the License is distributed on an
-// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-// KIND, either express or implied.  See the License for the
-// specific language governing permissions and limitations
-// under the License.` + "\n")
+// Modifications Copyright OpenSearch Contributors. See
+// GitHub history for details.` + "\n")
 	g.w("//\n// Code generated")
 	if EsVersion != "" {
 		g.w(fmt.Sprintf(" from specification version %s", EsVersion))
