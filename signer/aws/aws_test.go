1157d67a6abdee2bb7c7133247077091bc55e669
Vijayan Balasubramanian <balasvij@amazon.com>
2022-04-18 09:35:39 -0700

    Support service configuration
    Create new method to accept AWS service name
    as input.
    
    Signed-off-by: Vijayan Balasubramanian <balasvij@amazon.com>

--- signer/aws/aws_test.go
+++ signer/aws/aws_test.go
@@ -90,4 +90,41 @@ func TestV4Signer(t *testing.T) {
 		assert.NotEmpty(t, q.Get("Authorization"))
 		assert.NotEmpty(t, q.Get("X-Amz-Date"))
 	})
+
+	t.Run("sign request success with body for other AWS Services", func(t *testing.T) {
+		req, err := http.NewRequest(
+			http.MethodPost, "https://localhost:9200",
+			bytes.NewBuffer([]byte(`some data`)))
+		assert.NoError(t, err)
+		region := os.Getenv("AWS_REGION")
+		os.Setenv("AWS_REGION", "us-west-2")
+		defer func() {
+			os.Setenv("AWS_REGION", region)
+		}()
+		sessionOptions := session.Options{
+			Config: aws.Config{
+				Region:      aws.String("us-west-2"),
+				Credentials: credentials.NewStaticCredentials("AKID", "SECRET_KEY", "TOKEN"),
+			},
+		}
+		signer, err := NewSignerWithService(sessionOptions, "ec")
+		assert.NoError(t, err)
+		err = signer.SignRequest(req)
+		assert.NoError(t, err)
+		q := req.Header
+		assert.NotEmpty(t, q.Get("Authorization"))
+		assert.NotEmpty(t, q.Get("X-Amz-Date"))
+	})
+
+	t.Run("sign request failed due to invalid service", func(t *testing.T) {
+		sessionOptions := session.Options{
+			Config: aws.Config{
+				Region:      aws.String("us-west-2"),
+				Credentials: credentials.NewStaticCredentials("AKID", "SECRET_KEY", "TOKEN"),
+			},
+		}
+		_, err := NewSignerWithService(sessionOptions, "")
+		assert.EqualError(t, err, "service cannot be empty")
+
+	})
 }
