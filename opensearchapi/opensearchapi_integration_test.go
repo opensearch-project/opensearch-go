fa6cc9bed5ec90c6ec12572c0dfeecce54a4d0f5
Jakob Hahn <jakob.hahn@hetzner.com>
2023-03-17 17:35:12 +0100

    opensearchapi: Remove unneded IsError check
    Signed-off-by: Jakob Hahn <jakob.hahn@hetzner.com>

--- opensearchapi/opensearchapi_integration_test.go
+++ opensearchapi/opensearchapi_integration_test.go
@@ -24,6 +24,7 @@
 // specific language governing permissions and limitations
 // under the License.
 
+//go:build integration
 // +build integration
 
 package opensearchapi_test
@@ -54,10 +55,6 @@ func TestAPI(t *testing.T) {
 		}
 		defer res.Body.Close()
 
-		if res.IsError() {
-			t.Fatalf("Error response: %s", res.String())
-		}
-
 		var d map[string]interface{}
 		err = json.NewDecoder(res.Body).Decode(&d)
 		if err != nil {
@@ -77,10 +74,6 @@ func TestAPI(t *testing.T) {
 			t.Fatalf("Error getting the response: %s\n", err)
 		}
 		defer res.Body.Close()
-
-		if res.IsError() {
-			t.Fatalf("Error response: %s", res.String())
-		}
 
 		if !strings.HasPrefix(res.String(), "[200 OK] ---") {
 			t.Errorf("Unexpected response body: doesn't start with '[200 OK] ---'; %s", res.String())
@@ -121,10 +114,7 @@ func TestAPI(t *testing.T) {
 		if err != nil {
 			t.Fatalf("Failed to index data: %s", err)
 		}
-		res.Body.Close()
-		if res.IsError() {
-			t.Fatalf("Failed to index data: %s", res.Status())
-		}
+		defer res.Body.Close()
 
 		// Launch reindexing task with wait_for_completion=false
 		//
@@ -136,9 +126,6 @@ func TestAPI(t *testing.T) {
 		if err != nil {
 			t.Fatalf("Failed to reindex: %s", err)
 		}
-		if res.IsError() {
-			t.Fatalf("Failed to reindex: %s", res.Status())
-		}
 		time.Sleep(10 * time.Millisecond)
 
 		res, err = client.Tasks.List(client.Tasks.List.WithPretty())
@@ -146,9 +133,6 @@ func TestAPI(t *testing.T) {
 			t.Fatalf("ERROR: %s", err)
 		}
 		res.Body.Close()
-		if res.IsError() {
-			t.Fatalf("Failed to get tasks: %s", res.Status())
-		}
 
 		// Get the list of tasks
 		//
@@ -157,10 +141,6 @@ func TestAPI(t *testing.T) {
 			t.Fatalf("ERROR: %s", err)
 		}
 		defer res.Body.Close()
-
-		if res.IsError() {
-			t.Fatalf("Failed to get tasks: %s", res.Status())
-		}
 
 		type task struct {
 			Node        string
@@ -206,10 +186,7 @@ func TestAPI(t *testing.T) {
 						if err != nil {
 							t.Fatalf("ERROR: %s", err)
 						}
-						res.Body.Close()
-						if res.IsError() {
-							t.Fatalf("Failed to cancel task: %s", res)
-						}
+						defer res.Body.Close()
 					}
 				}
 			}
