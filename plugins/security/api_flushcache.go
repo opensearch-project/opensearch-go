27e261cf0f97d9ed86477f40572f29b1d0398b7f
Jakob Hahn <jakob.hahn@hetzner.com>
2024-04-03 11:14:51 +0200

    plugins/security: add flushcache functions
    Signed-off-by: Jakob Hahn <jakob.hahn@hetzner.com>

---
+++ plugins/security/api_flushcache.go
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: Apache-2.0
+//
+// The OpenSearch Contributors require contributions made to
+// this file be licensed under the Apache-2.0 license or a
+// compatible open source license.
+
+package security
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/opensearch-project/opensearch-go/v3"
+)
+
+// FlushCache executes a flush cache request with the optional FlushCacheReq
+func (c Client) FlushCache(ctx context.Context, req *FlushCacheReq) (FlushCacheResp, error) {
+	if req == nil {
+		req = &FlushCacheReq{}
+	}
+
+	var (
+		data FlushCacheResp
+		err  error
+	)
+	if data.response, err = c.do(ctx, req, &data); err != nil {
+		return data, err
+	}
+
+	return data, nil
+}
+
+// FlushCacheReq represents possible options for the clush cache request
+type FlushCacheReq struct {
+	Header http.Header
+}
+
+// GetRequest returns the *http.Request that gets executed by the client
+func (r FlushCacheReq) GetRequest() (*http.Request, error) {
+	return opensearch.BuildRequest(
+		"DELETE",
+		"/_plugins/_security/api/cache",
+		nil,
+		make(map[string]string),
+		r.Header,
+	)
+}
+
+// FlushCacheResp represents the returned struct of the flush cache response
+type FlushCacheResp struct {
+	Status   string `json:"status"`
+	Message  string `json:"message"`
+	response *opensearch.Response
+}
+
+// Inspect returns the Inspect type containing the raw *opensearch.Reponse
+func (r FlushCacheResp) Inspect() Inspect {
+	return Inspect{Response: r.response}
+}
---
+++ plugins/security/api_flushcache_test.go
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: Apache-2.0
+//
+// The OpenSearch Contributors require contributions made to
+// this file be licensed under the Apache-2.0 license or a
+// compatible open source license.
+//
+//go:build integration
+
+package security_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	ostest "github.com/opensearch-project/opensearch-go/v3/internal/test"
+	ossectest "github.com/opensearch-project/opensearch-go/v3/plugins/security/internal/test"
+)
+
+func TestFlushCache(t *testing.T) {
+	ostest.SkipIfNotSecure(t)
+	client, err := ossectest.NewClient()
+	require.Nil(t, err)
+
+	failingClient, err := ossectest.CreateFailingClient()
+	require.Nil(t, err)
+
+	t.Run("without request", func(t *testing.T) {
+		resp, err := client.FlushCache(nil, nil)
+		require.Nil(t, err)
+		assert.NotNil(t, resp)
+		ostest.CompareRawJSONwithParsedJSON(t, resp, resp.Inspect().Response)
+	})
+
+	t.Run("inspect", func(t *testing.T) {
+		res, err := failingClient.FlushCache(nil, nil)
+		assert.NotNil(t, err)
+		assert.NotNil(t, res)
+		ossectest.VerifyInspect(t, res.Inspect())
+	})
+}
