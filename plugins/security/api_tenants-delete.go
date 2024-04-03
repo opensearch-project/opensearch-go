1f72680e52a68f33916e9e6a1dbdcb2b88ee4fa9
Jakob Hahn <jakob.hahn@hetzner.com>
2024-04-03 11:13:16 +0200

    plugins/security: add tenants functions
    Signed-off-by: Jakob Hahn <jakob.hahn@hetzner.com>

---
+++ plugins/security/api_tenants-delete.go
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: Apache-2.0
+//
+// The OpenSearch Contributors require contributions made to
+// this file be licensed under the Apache-2.0 license or a
+// compatible open source license.
+
+package security
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/opensearch-project/opensearch-go/v3"
+)
+
+// TenantsDeleteReq represents possible options for the tenants delete request
+type TenantsDeleteReq struct {
+	Tenant string
+
+	Header http.Header
+}
+
+// GetRequest returns the *http.Request that gets executed by the client
+func (r TenantsDeleteReq) GetRequest() (*http.Request, error) {
+	return opensearch.BuildRequest(
+		"DELETE",
+		fmt.Sprintf("/_plugins/_security/api/tenants/%s", r.Tenant),
+		nil,
+		make(map[string]string),
+		r.Header,
+	)
+}
+
+// TenantsDeleteResp represents the returned struct of the tenants delete response
+type TenantsDeleteResp struct {
+	Status   string `json:"status"`
+	Message  string `json:"message"`
+	response *opensearch.Response
+}
+
+// Inspect returns the Inspect type containing the raw *opensearch.Reponse
+func (r TenantsDeleteResp) Inspect() Inspect {
+	return Inspect{Response: r.response}
+}
---
+++ plugins/security/api_tenants.go
@@ -0,0 +1,71 @@
+// SPDX-License-Identifier: Apache-2.0
+//
+// The OpenSearch Contributors require contributions made to
+// this file be licensed under the Apache-2.0 license or a
+// compatible open source license.
+
+package security
+
+import (
+	"context"
+)
+
+type tenantsClient struct {
+	apiClient *Client
+}
+
+// Get executes a get tenants request with the optional TenantsGetReq
+func (c tenantsClient) Get(ctx context.Context, req *TenantsGetReq) (TenantsGetResp, error) {
+	if req == nil {
+		req = &TenantsGetReq{}
+	}
+
+	var (
+		data TenantsGetResp
+		err  error
+	)
+	if data.response, err = c.apiClient.do(ctx, req, &data); err != nil {
+		return data, err
+	}
+
+	return data, nil
+}
+
+// Put executes a put tenants request with the required TenantsPutReq
+func (c tenantsClient) Put(ctx context.Context, req TenantsPutReq) (TenantsPutResp, error) {
+	var (
+		data TenantsPutResp
+		err  error
+	)
+	if data.response, err = c.apiClient.do(ctx, req, &data); err != nil {
+		return data, err
+	}
+
+	return data, nil
+}
+
+// Delete executes a delete tenants request with the required TenantsDeleteReq
+func (c tenantsClient) Delete(ctx context.Context, req TenantsDeleteReq) (TenantsDeleteResp, error) {
+	var (
+		data TenantsDeleteResp
+		err  error
+	)
+	if data.response, err = c.apiClient.do(ctx, req, &data); err != nil {
+		return data, err
+	}
+
+	return data, nil
+}
+
+// Patch executes a patch tenants request with the required TenantsPatchReq
+func (c tenantsClient) Patch(ctx context.Context, req TenantsPatchReq) (TenantsPatchResp, error) {
+	var (
+		data TenantsPatchResp
+		err  error
+	)
+	if data.response, err = c.apiClient.do(ctx, req, &data); err != nil {
+		return data, err
+	}
+
+	return data, nil
+}
