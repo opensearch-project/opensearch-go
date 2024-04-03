76a484ca3f4a7efe1df479241fcf4065beaa0167
Jakob Hahn <jakob.hahn@hetzner.com>
2024-04-03 11:13:37 +0200

    plugins/security: add securityconfig functions
    Signed-off-by: Jakob Hahn <jakob.hahn@hetzner.com>

---
+++ plugins/security/api_securityconfig-put.go
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: Apache-2.0
+//
+// The OpenSearch Contributors require contributions made to
+// this file be licensed under the Apache-2.0 license or a
+// compatible open source license.
+
+package security
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+
+	"github.com/opensearch-project/opensearch-go/v3"
+)
+
+// ConfigPutReq represents possible options for the securityconfig get request
+type ConfigPutReq struct {
+	Body ConfigPutBody
+
+	Header http.Header
+}
+
+// GetRequest returns the *http.Request that gets executed by the client
+func (r ConfigPutReq) GetRequest() (*http.Request, error) {
+	body, err := json.Marshal(r.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return opensearch.BuildRequest(
+		"PUT",
+		"/_plugins/_security/api/securityconfig/config",
+		bytes.NewReader(body),
+		make(map[string]string),
+		r.Header,
+	)
+}
+
+// ConfigPutBody represents the request body for ConfigPutReq
+type ConfigPutBody struct {
+	Dynamic ConfigDynamic `json:"dynamic"`
+}
+
+// ConfigPutResp represents the returned struct of the securityconfig get response
+type ConfigPutResp struct {
+	Status   string `json:"status"`
+	Message  string `json:"message"`
+	response *opensearch.Response
+}
+
+// Inspect returns the Inspect type containing the raw *opensearch.Reponse
+func (r ConfigPutResp) Inspect() Inspect {
+	return Inspect{Response: r.response}
+}
