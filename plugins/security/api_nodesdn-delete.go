905dec72ca735bbcbaddb9c2f37a05569afabb91
Jakob Hahn <jakob.hahn@hetzner.com>
2024-04-03 11:14:15 +0200

    plugins/security: add nodesdn functions
    Signed-off-by: Jakob Hahn <jakob.hahn@hetzner.com>

---
+++ plugins/security/api_nodesdn-delete.go
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: Apache-2.0
+//
+// The OpenSearch Contributors require contributions made to
+// this file be licensed under the Apache-2.0 license or a
+// compatible open source license.
+
+package security
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/opensearch-project/opensearch-go/v3"
+)
+
+// NodesDNDeleteReq represents possible options for the nodesdn delete request
+type NodesDNDeleteReq struct {
+	Cluster string
+
+	Header http.Header
+}
+
+// GetRequest returns the *http.Request that gets executed by the client
+func (r NodesDNDeleteReq) GetRequest() (*http.Request, error) {
+	return opensearch.BuildRequest(
+		"DELETE",
+		fmt.Sprintf("/_plugins/_security/api/nodesdn/%s", r.Cluster),
+		nil,
+		make(map[string]string),
+		r.Header,
+	)
+}
+
+// NodesDNDeleteResp represents the returned struct of the nodesdn delete response
+type NodesDNDeleteResp struct {
+	Status   string `json:"status"`
+	Message  string `json:"message"`
+	response *opensearch.Response
+}
+
+// Inspect returns the Inspect type containing the raw *opensearch.Reponse
+func (r NodesDNDeleteResp) Inspect() Inspect {
+	return Inspect{Response: r.response}
+}
