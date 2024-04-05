3d73e7b9e707a5bd28fa0b3aa7e96a54d21a1b6d
Jakob Hahn <jakob.hahn@hetzner.com>
2024-04-05 16:14:57 +0200

    error_test: adjust to new error types
    Signed-off-by: Jakob Hahn <jakob.hahn@hetzner.com>

--- error_test.go
+++ error_test.go
@@ -6,10 +6,10 @@
 
 //go:build !integration
 
-//nolint:testpackage // need to use the parseError function defined in opensearchapi.go
-package opensearchapi
+package opensearch_test
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
@@ -25,11 +25,12 @@ import (
 )
 
 func TestError(t *testing.T) {
-	t.Run("Error okay", func(t *testing.T) {
-		resp := &opensearch.Response{
-			StatusCode: http.StatusBadRequest,
-			Body: io.NopCloser(
-				strings.NewReader(`
+	t.Run("StructError", func(t *testing.T) {
+		t.Run("Ok", func(t *testing.T) {
+			resp := &opensearch.Response{
+				StatusCode: http.StatusBadRequest,
+				Body: io.NopCloser(
+					strings.NewReader(`
 					{
 						"error":{
 							"root_cause":[{
@@ -45,26 +46,65 @@ func TestError(t *testing.T) {
 						},
 						"status":400
 					}`),
-			),
-		}
-		assert.True(t, resp.IsError())
-		err := parseError(resp)
-		var testError Error
-		require.True(t, errors.As(err, &testError))
-		assert.Equal(t, http.StatusBadRequest, testError.Status)
-		assert.Equal(t, "resource_already_exists_exception", testError.Err.Type)
-		assert.Equal(t, "index [test/HU2mN_RMRXGcS38j3yV-VQ] already exists", testError.Err.Reason)
-		assert.Equal(t, "test", testError.Err.Index)
-		assert.Equal(t, "HU2mN_RMRXGcS38j3yV-VQ", testError.Err.IndexUUID)
-		assert.NotNil(t, testError.Err.RootCause)
-		assert.Equal(t, "resource_already_exists_exception", testError.Err.RootCause[0].Type)
-		assert.Equal(t, "index [test/HU2mN_RMRXGcS38j3yV-VQ] already exists", testError.Err.RootCause[0].Reason)
-		assert.Equal(t, "test", testError.Err.RootCause[0].Index)
-		assert.Equal(t, "HU2mN_RMRXGcS38j3yV-VQ", testError.Err.RootCause[0].IndexUUID)
-		_ = fmt.Sprintf("%s", err)
+				),
+			}
+			assert.True(t, resp.IsError())
+			err := opensearch.ParseError(resp)
+			var testError *opensearch.StructError
+			require.True(t, errors.As(err, &testError))
+			assert.Equal(t, http.StatusBadRequest, testError.Status)
+			assert.Equal(t, "resource_already_exists_exception", testError.Err.Type)
+			assert.Equal(t, "index [test/HU2mN_RMRXGcS38j3yV-VQ] already exists", testError.Err.Reason)
+			assert.Equal(t, "test", testError.Err.Index)
+			assert.Equal(t, "HU2mN_RMRXGcS38j3yV-VQ", testError.Err.IndexUUID)
+			assert.NotNil(t, testError.Err.RootCause)
+			assert.Equal(t, "resource_already_exists_exception", testError.Err.RootCause[0].Type)
+			assert.Equal(t, "index [test/HU2mN_RMRXGcS38j3yV-VQ] already exists", testError.Err.RootCause[0].Reason)
+			assert.Equal(t, "test", testError.Err.RootCause[0].Index)
+			assert.Equal(t, "HU2mN_RMRXGcS38j3yV-VQ", testError.Err.RootCause[0].IndexUUID)
+			_ = fmt.Sprintf("%s", err)
+		})
+
+		t.Run("Unmarshal errors", func(t *testing.T) {
+			t.Run("dummy", func(t *testing.T) {
+				reader := io.NopCloser(
+					strings.NewReader(`
+					{
+						"status": "400"
+					}`),
+				)
+				body, err := io.ReadAll(reader)
+				require.Nil(t, err)
+
+				var errStruct *opensearch.StructError
+				err = json.Unmarshal(body, &errStruct)
+				assert.NotNil(t, err)
+
+				var jsonError *json.UnmarshalTypeError
+				assert.True(t, errors.As(err, &jsonError))
+			})
+			t.Run("string", func(t *testing.T) {
+				reader := io.NopCloser(
+					strings.NewReader(`
+					{
+						"error": 0,
+						"status": 500
+					}`),
+				)
+				body, err := io.ReadAll(reader)
+				require.Nil(t, err)
+
+				var errStruct *opensearch.StructError
+				err = json.Unmarshal(body, &errStruct)
+				assert.NotNil(t, err)
+
+				var errStr *opensearch.StringError
+				require.True(t, errors.As(err, &errStr))
+			})
+		})
 	})
 
-	t.Run("StringError okay", func(t *testing.T) {
+	t.Run("StringError", func(t *testing.T) {
 		resp := &opensearch.Response{
 			StatusCode: http.StatusMethodNotAllowed,
 			Body: io.NopCloser(
@@ -76,13 +116,71 @@ func TestError(t *testing.T) {
 			),
 		}
 		assert.True(t, resp.IsError())
-		err := parseError(resp)
-		var testError StringError
+		err := opensearch.ParseError(resp)
+		var testError *opensearch.StringError
 		require.True(t, errors.As(err, &testError))
 		assert.Equal(t, http.StatusMethodNotAllowed, testError.Status)
 		assert.Contains(t, testError.Err, "Incorrect HTTP method for uri")
 		_ = fmt.Sprintf("%s", testError)
 	})
+
+	t.Run("Error", func(t *testing.T) {
+		resp := &opensearch.Response{
+			StatusCode: http.StatusBadRequest,
+			Body: io.NopCloser(
+				strings.NewReader(`
+					{
+					  "error": "no handler found for uri [/_plugins/_security/xxx] and method [GET]"
+					}`),
+			),
+		}
+		assert.True(t, resp.IsError())
+		err := opensearch.ParseError(resp)
+		var testError *opensearch.Error
+		require.True(t, errors.As(err, &testError))
+		assert.Contains(t, testError.Err, "no handler found for uri [/_plugins/_security/xxx] and method [GET]")
+		_ = fmt.Sprintf("%s", testError)
+	})
+
+	t.Run("ReasonError", func(t *testing.T) {
+		resp := &opensearch.Response{
+			StatusCode: http.StatusBadRequest,
+			Body: io.NopCloser(
+				strings.NewReader(`
+					{
+					  "status": "error",
+					  "reason": "Invalid configuration",
+					  "invalid_keys": {
+					    "keys": "dynamic"
+					  }
+					}`),
+			),
+		}
+		assert.True(t, resp.IsError())
+		err := opensearch.ParseError(resp)
+		var testError *opensearch.ReasonError
+		require.True(t, errors.As(err, &testError))
+		assert.Equal(t, "error", testError.Status)
+		assert.Contains(t, testError.Reason, "Invalid configuration")
+		_ = fmt.Sprintf("%s", testError)
+	})
+
+	t.Run("MessageError", func(t *testing.T) {
+		resp := &opensearch.Response{
+			StatusCode: http.StatusBadRequest,
+			Body: io.NopCloser(
+				strings.NewReader(`{"status":"BAD_REQUEST","message":"Wrong request body"}`),
+			),
+		}
+		assert.True(t, resp.IsError())
+		err := opensearch.ParseError(resp)
+		var testError *opensearch.MessageError
+		require.True(t, errors.As(err, &testError))
+		assert.Equal(t, "BAD_REQUEST", testError.Status)
+		assert.Contains(t, testError.Message, "Wrong request body")
+		_ = fmt.Sprintf("%s", testError)
+	})
+
 	t.Run("Unexpected", func(t *testing.T) {
 		cases := []struct {
 			Name         string
@@ -95,7 +193,7 @@ func TestError(t *testing.T) {
 					StatusCode: http.StatusMethodNotAllowed,
 					Body:       io.NopCloser(strings.NewReader(`"Test - Trigger an error"`)),
 				},
-				WantedErrors: []error{ErrJSONUnmarshalBody},
+				WantedErrors: []error{opensearch.ErrJSONUnmarshalBody},
 			},
 			{
 				Name: "response string",
@@ -103,15 +201,7 @@ func TestError(t *testing.T) {
 					StatusCode: http.StatusForbidden,
 					Body:       io.NopCloser(strings.NewReader(`"Test - Trigger an error"`)),
 				},
-				WantedErrors: []error{ErrJSONUnmarshalBody},
-			},
-			{
-				Name: "error field string",
-				Resp: &opensearch.Response{
-					StatusCode: http.StatusForbidden,
-					Body:       io.NopCloser(strings.NewReader(`{"error":"test","status":403}`)),
-				},
-				WantedErrors: []error{ErrJSONUnmarshalBody},
+				WantedErrors: []error{opensearch.ErrJSONUnmarshalBody},
 			},
 			{
 				Name: "error field object",
@@ -119,7 +209,7 @@ func TestError(t *testing.T) {
 					StatusCode: http.StatusForbidden,
 					Body:       io.NopCloser(strings.NewReader(`{"error":{"test": "test"},"status":403}`)),
 				},
-				WantedErrors: []error{ErrJSONUnmarshalBody, ErrUnknownOpensearchError},
+				WantedErrors: []error{opensearch.ErrJSONUnmarshalBody, opensearch.ErrUnknownOpensearchError},
 			},
 			{
 				Name: "response json",
@@ -127,7 +217,7 @@ func TestError(t *testing.T) {
 					StatusCode: http.StatusNotFound,
 					Body:       io.NopCloser(strings.NewReader(`{"_index":"index","_id":"2","matched":false}`)),
 				},
-				WantedErrors: []error{ErrJSONUnmarshalBody, ErrJSONUnmarshalBody},
+				WantedErrors: []error{opensearch.ErrUnknownOpensearchError},
 			},
 			{
 				Name: "io read error",
@@ -135,18 +225,18 @@ func TestError(t *testing.T) {
 					StatusCode: http.StatusBadRequest,
 					Body:       io.NopCloser(iotest.ErrReader(errors.New("io reader test"))),
 				},
-				WantedErrors: []error{ErrReadBody},
+				WantedErrors: []error{opensearch.ErrReadBody},
 			},
 			{
 				Name:         "empty body",
 				Resp:         &opensearch.Response{StatusCode: http.StatusBadRequest},
-				WantedErrors: []error{ErrUnexpectedEmptyBody},
+				WantedErrors: []error{opensearch.ErrUnexpectedEmptyBody},
 			},
 		}
 
 		for _, tt := range cases {
 			t.Run(tt.Name, func(t *testing.T) {
-				err := parseError(tt.Resp)
+				err := opensearch.ParseError(tt.Resp)
 				for _, wantedError := range tt.WantedErrors {
 					assert.True(t, errors.Is(err, wantedError))
 				}
