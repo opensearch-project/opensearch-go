040f709b3b46c2c20bebbf8383cb12afdcc506d2
tim-holdaway-clever <159181128+tim-holdaway-clever@users.noreply.github.com>
2025-01-18 05:45:22 -0800

    Add Highlight field to SearchHit struct (#654)
    * Add Highlight field to SearchHit struct
    
    Signed-off-by: Tim Holdaway <timothy.holdaway@clever.com>
    
    * Update changelog
    
    Signed-off-by: Tim Holdaway <timothy.holdaway@clever.com>
    
    * Update SearchTemplate struct with Status field
    
    This field is returned since OpenSearch 2.18
    
    Signed-off-by: Tim Holdaway <timothy.holdaway@clever.com>
    
    * Update NodeStatsstruct with RemoteStore field
    
    This field is returned since OpenSearch 2.18.
    (see https://github.com/opensearch-project/OpenSearch/pull/15611)
    
    Signed-off-by: Tim Holdaway <timothy.holdaway@clever.com>
    
    * Always attempt to pull the docker base image for integration tests.
    
    Otherwise we can end up with a cached version of opensearch:latest that is not the current latest, and would use that instead,
    resulting in tests running against a different version than in the CI build.
    
    Signed-off-by: Tim Holdaway <timothy.holdaway@clever.com>
    
    ---------
    
    Signed-off-by: Tim Holdaway <timothy.holdaway@clever.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,7 @@ Inspired from [Keep a Changelog](https://keepachangelog.com/en/1.0.0/)
 ## [Unreleased]
 
 ### Added
+- Adds `Highlight` field to `SearchHit` ([#654](https://github.com/opensearch-project/opensearch-go/pull/654))
 
 ### Changed
 
--- opensearchapi/api_search_test.go
+++ opensearchapi/api_search_test.go
@@ -181,4 +181,28 @@ func TestSearch(t *testing.T) {
 		require.Nil(t, err)
 		assert.NotEmpty(t, resp.Suggest)
 	})
+
+	t.Run("request with highlight", func(t *testing.T) {
+		resp, err := client.Search(
+			nil,
+			&opensearchapi.SearchReq{
+				Indices: []string{index},
+				Body: strings.NewReader(`{
+					"query": {
+						"match": {
+							"foo": "bar"
+						}
+					},
+					"highlight": {
+						"fields": {
+							"foo": {}
+						}
+					}
+				}`),
+			},
+		)
+		require.Nil(t, err)
+		assert.NotEmpty(t, resp.Hits.Hits)
+		assert.Equal(t, map[string][]string{"foo": []string{"<em>bar</em>"}}, resp.Hits.Hits[0].Highlight)
+	})
 }
