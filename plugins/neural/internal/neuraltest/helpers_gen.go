d10bb1ca0c0dc4d4e1195e2d043daa3674b5415a
Ryan Yuan <ryan.yuan@crowdstrike.com>
2026-08-07 14:00:01 +1000

    Regenerate plugin test helpers with client cleanup
    Output of `make gen` after the plugin test-helper template change. No
    hand edits. Adds 50 t.Cleanup calls across the 25 generated
    plugins/*/internal/*test/helpers_gen.go files.
    
    Signed-off-by: Ryan Yuan <ryan.yuan@crowdstrike.com>
    Signed-off-by: Sean Chittenden <sean.chittenden@crowdstrike.com>

--- plugins/neural/internal/neuraltest/helpers_gen.go
+++ plugins/neural/internal/neuraltest/helpers_gen.go
@@ -31,6 +31,7 @@ func NewClient(t *testing.T) (*neural.Client, error) {
 	if err != nil {
 		return nil, err
 	}
+	t.Cleanup(func() { _ = osClient.Close() })
 	return neural.NewClient(osClient), nil
 }
 
@@ -58,6 +59,7 @@ func CreateFailingClient(t *testing.T) (*neural.Client, error) {
 	if err != nil {
 		return nil, err
 	}
+	t.Cleanup(func() { _ = osClient.Close() })
 	return neural.NewClient(osClient), nil
 }
 
--- plugins/notifications/internal/notificationstest/helpers_gen.go
+++ plugins/notifications/internal/notificationstest/helpers_gen.go
@@ -31,6 +31,7 @@ func NewClient(t *testing.T) (*notifications.Client, error) {
 	if err != nil {
 		return nil, err
 	}
+	t.Cleanup(func() { _ = osClient.Close() })
 	return notifications.NewClient(osClient), nil
 }
 
@@ -58,6 +59,7 @@ func CreateFailingClient(t *testing.T) (*notifications.Client, error) {
 	if err != nil {
 		return nil, err
 	}
+	t.Cleanup(func() { _ = osClient.Close() })
 	return notifications.NewClient(osClient), nil
 }
 
